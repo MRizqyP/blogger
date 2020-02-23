c0b2286db50a0c8bac3c70f1319754d0c618b2ff
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-23 21:11:38 +0700

    baru sampe upload img

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">HY MY NAME IS RIZQY</div>;
+}
+
+export default App;
